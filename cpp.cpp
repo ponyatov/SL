c7c323c0a8d15523fba9fabca05335a253e12b58
Dmitry Ponyatov <dponyatov@gmail.com>
2018-07-22 15:59:20 +0400

    v FIND/EXECUTE exec
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,28 +1,73 @@
 #include "hpp.hpp"
 
-int main(int argc, char *argv[]) { env_init(); return yyparse(); }
+// very simple system start: just run interpreter
+int main(int argc, char *argv[]) { init(); return yyparse(); }
 
+/// syntax error template
 #define YYERR "\n\n" << yylineno << ": " << msg << " [" << yytext << "]\n\n"
+/// syntax error callback
 void yyerror(string msg) { cout << YYERR; cerr << YYERR; exit(-1); }
 
-Sym::Sym(string T, string V) { type = T; value = V; }
+Sym::Sym(string T, string V)    { type = T; value = V; }
 Sym::Sym(string V):Sym("sym",V) {}
 
-string Sym::dump(int depth) { return "<"+type+":"+value+">"; }
+Sym* Sym::lookup(string name) {
+	auto found = attr.find(name);
+	if (found == attr.end())	return NULL;
+	else						return found->second;
+}
 
-Sym* Sym::eval() { return this; }
+void Sym::push(Sym*o)	{ nest.push_back(o); }
+Sym* Sym::pop()			{ Sym*o = nest.back(); nest.pop_back();	return o; }
+
+string Sym::dump(int depth, string prefix) {
+	string S = pad(depth) + head(prefix);
+	for (auto i = attr.begin(), e = attr.end(); i != e; i++)
+		S += i->second->dump(depth + 1, prefix = i->first + " = ");
+	return S;
+}
+
+string Sym::head(string prefix) {
+	ostringstream os;
+	os << prefix << '<' << type << ':' << value << "> @" << this;
+	return os.str();
+}
+
+string Sym::pad(int N) {
+	string S; S += '\n';
+	for (int i = 0; i < N; i++) S += '\t';
+	return S;
+}
+
+void Sym::exec(void) { Stack->push(this); }
 
 Env::Env(string V):Sym("env",V) {}
 
 Env *env = new Env("global");
 
-void dump(void) { cout << env->dump(); }
-
-void env_init() {
-	env->attr["?"] = new Fn("dump",dump);
+void DUMP(void) {
+	cout << env->dump();
+	cout << Stack->dump();
 }
 
 Fn::Fn(string V, void(*F)(void)):Sym("fn",V) { fn = F; }
 
-Sym* Fn::eval() { fn(); return this; }
+void Fn::exec(void) { fn(); }
 
+Sym *Stack = new Sym("stack","DATA");
+
+void init() {
+	env->attr["?"] = new Fn("dump",DUMP);
+}
+
+bool FIND(void) {
+	Sym* name  = Stack->pop();
+	Sym* found = env->lookup(name->value);
+	if (found) {
+		Stack->push(found); return true;
+	} else {
+		Stack->push(name); return false;
+	}
+}
+
+void EXECUTE(void) { Stack->pop()->exec(); }
--- hpp.hpp
+++ hpp.hpp
@@ -2,42 +2,65 @@
 #define _H_HPP
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <cassert>
 #include <map>
 #include <vector>
 using namespace std;
 
-struct Sym {											// base symbol class
-	string type;										// type tag
-	string value;										// single value
-	Sym(string,string);									// <T:V> constructor
-	Sym(string);										// sym:V constructor
-	map<string,Sym*> attr;								// named attr{}ibutes
-	vector<Sym*> nest;									// nest[]ed elements
-	virtual string dump(int depth=0);					// dump in tree form
-	virtual Sym* eval();								// compute/evaluate
+struct Sym {										// base symbol class
+	string type;									// type tag
+	string value;									// single value
+	Sym(string,string);								// <T:V> constructor
+	Sym(string);									// sym:V constructor
+
+	map<string,Sym*> attr;							// named attr{}ibutes
+	Sym* lookup(string name);
+
+	vector<Sym*> nest;								// === nest[]ed elements
+	void push(Sym*);								// push object
+	Sym* pop();										// pop top object
+
+	virtual string dump(int depth=0, string prefix=""); // dump in tree form
+	virtual string head(string prefix="");
+	string pad(int N);
+
+	virtual void exec(void);						// execute
 };
 
-struct Env:Sym { Env(string V); };						// environment
+struct Env:Sym { Env(string V); };					// environment
 
-extern Env* env;										// global env
-extern void env_init();
+extern Env* env;									// global env
+extern void init();
 
-struct Fn:Sym {											// VM function
+struct Fn:Sym {										// VM function
 	Fn(string V, void(*F)(void) );
-	void(*fn)(void);									// pointer to VM fn
-	Sym* eval();
+	void(*fn)(void);								// pointer to VM fn
+	void exec(void);
 };
 
-														// === parser interface
-extern int yylex();										// lexer get next token
-extern int yylineno;									// current line number
-extern char* yytext;									// current lexeme value
+struct Stack:Sym { Stack(string); };				// stack container
+
+struct Map:Sym { Map(string); };					// associative container
+
+													// === parser interface
+extern int yylex();									// lexer get next token
+extern int yylineno;								// current line number
+extern char* yytext;								// current lexeme value
 #define TOC(C,T) { yylval.o = new C(yytext); return T; }/* token macro */
-extern int yyparse();									// parser
-extern void yyerror(string);							// syntax error callback
-#include "ypp.tab.hpp"									// token definitions
+extern int yyparse();								// parser
+extern void yyerror(string);						// syntax error callback
+#include "ypp.tab.hpp"								// token definitions
+
+/// Object Virtual Machine
+
+extern Sym *Stack;									// shared data stack
+
+/// commands
+
+extern bool FIND(void);
+extern void EXECUTE(void);
 
 #endif // _H_HPP
 
